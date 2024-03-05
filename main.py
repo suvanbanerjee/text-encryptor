ff01309d4cebf90b346b9e26c5b807275f96058c
suvanbanerjee <banerjeesuvan@gmail.com>
2024-03-05 16:39:17 +0530

    basic frontend completed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyscript import document
 
 def encrypt(a):
     print(a)
-    input_text = document.querySelector("#plain_text")
+    input_text = document.querySelector("#text")
     input_text = input_text.value
     key = document.querySelector("#pass")
     key = key.value
@@ -13,22 +13,20 @@ def encrypt(a):
     encrypted_message = data_encrypter.GetEncryptedData()
     output_div = document.querySelector("#output")
     encrypted_message = bytes.hex(encrypted_message)
-    output_div.innerText = "Encrypted Message: " + str(encrypted_message) + "\n" + "Key: " + str(key)
-    print(encrypted_message)
+    output_div.innerText = "Encrypted Message\n\n" + str(encrypted_message)
 
 def decrypt(a):
-    input_text = document.querySelector("#decrypt_text")
+    input_text = document.querySelector("#text")
     input_text = input_text.value
-    key = document.querySelector("#decrypt_pass")
+    key = document.querySelector("#pass")
     key = key.value
     try:
         ScryptDefault = aes_cipher.Scrypt(16384, 8, 8)
         data_encrypter = aes_cipher.DataDecrypter(ScryptDefault)
         data_encrypter.Decrypt(bytes.fromhex(input_text), key)
         decrypted_message = data_encrypter.GetDecryptedData()
-        output_div = document.querySelector("#output2")
-        output_div.innerText = "Decrypted Message: " + str(decrypted_message)[2:-1]
+        output_div = document.querySelector("#output")
+        output_div.innerText = "Decrypted Message\n\n" + str(decrypted_message)[2:-1]
     except:
-        output_div = document.querySelector("#output2")
+        output_div = document.querySelector("#output")
         output_div.innerText = "Invalid Key or Encrypted Message"
-    print(decrypted_message)
