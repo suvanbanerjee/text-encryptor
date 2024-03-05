83a8f5ca779902c37a2376a4c144916cb127fd7f
suvanbanerjee <banerjeesuvan@gmail.com>
2024-03-05 14:02:41 +0530

    added webapp skeleton

--- main.py
+++ main.py
@@ -7,8 +7,28 @@ def encrypt(a):
     input_text = input_text.value
     key = document.querySelector("#pass")
     key = key.value
-    data_encrypter = aes_cipher.DataEncrypter()
-    data_encrypter.Encrypt(input_text.encode(), key)
+    ScryptDefault = aes_cipher.Scrypt(16384, 8, 8)
+    data_encrypter = aes_cipher.DataEncrypter(ScryptDefault)
+    data_encrypter.Encrypt(input_text, key)
     encrypted_message = data_encrypter.GetEncryptedData()
     output_div = document.querySelector("#output")
-    output_div.innerText = "Encrypted Message: " + str(encrypted_message)[2:-1] + "\n" + "Key: " + str(key)[2:-1] + "\n"
+    encrypted_message = bytes.hex(encrypted_message)
+    output_div.innerText = "Encrypted Message: " + str(encrypted_message) + "\n" + "Key: " + str(key)
+    print(encrypted_message)
+
+def decrypt(a):
+    input_text = document.querySelector("#decrypt_text")
+    input_text = input_text.value
+    key = document.querySelector("#decrypt_pass")
+    key = key.value
+    try:
+        ScryptDefault = aes_cipher.Scrypt(16384, 8, 8)
+        data_encrypter = aes_cipher.DataDecrypter(ScryptDefault)
+        data_encrypter.Decrypt(bytes.fromhex(input_text), key)
+        decrypted_message = data_encrypter.GetDecryptedData()
+        output_div = document.querySelector("#output2")
+        output_div.innerText = "Decrypted Message: " + str(decrypted_message)[2:-1]
+    except:
+        output_div = document.querySelector("#output2")
+        output_div.innerText = "Invalid Key or Encrypted Message"
+    print(decrypted_message)
